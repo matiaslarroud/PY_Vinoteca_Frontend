74d3754f6a5bdea0212a2aebe706eb2498962ae0
matiaslarroud <matutedel30@outlook.com>
2025-06-29 17:09:46 -0300

    AGREGAMOS VARIABLE DE ENTORNO

--- pages/products/createProduct.js
+++ pages/products/createProduct.js
@@ -20,7 +20,7 @@ const formProducto = () => {
 
          console.log(product)
 
-         fetch('http://localhost:5000/api/v1/products',
+         fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/products`,
             {
                 method: 'POST',
                 headers: {'Content-Type':'application/json'},
