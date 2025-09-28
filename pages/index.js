21feba519bf566f57d0b4fc70998936c91bd0b42
matiaslarroud <matutedel30@outlook.com>
2025-09-28 03:39:53 -0300

    CAMBIOS 28-09-2025

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
           <span>Proveedores</span>
         </Link>
       </div>
-      <div className="boton-acceso">
+      {/* <div className="boton-acceso">
         <Link href="/gestion/createBodega" className="boton-acceso">
           <FaChartBar className="icono" />
           <span>Reportes</span>
@@ -50,7 +50,7 @@ export default function Home() {
           <FaCog className="icono" />
           <span>Gestion</span>
         </Link>
-      </div>
+      </div> */}
   </div>
    <div className="lowstock-wrapper">
       <LowStockGrid
